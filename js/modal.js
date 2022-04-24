0ae6937ba56de82d177c4e4f8c75b55701731162
AlexEgupov <81446686+AlexEgupov@users.noreply.github.com>
2022-04-25 02:07:04 +0300

    3

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,21 @@
 const modalBtn = document.querySelector('.modal__button')
+const courseBtn = document.querySelector('.course__button')
+
 const modal = document.querySelector('.modal')
+const modalInner = modal.querySelector('.modal__inner')
 
-modalBtn.addEventListener('click', () => {
-    modal.style.display = 'flex'
-})
+// Открытие модального окна
+const modalOpen = (button) => {
 
+    button.addEventListener('click', () => {
+        modal.style.display = 'flex'
+    })
+}
+
+modalOpen(modalBtn)
+modalOpen(courseBtn)
+
+// Закрытие модальног окна
 modal.addEventListener('click', (event) => {
     const modalContent = event.target.closest('.modal__inner')
 
@@ -12,3 +23,34 @@ modal.addEventListener('click', (event) => {
         modal.style.display = ''
     }
 })
+
+
+
+// Добавление крестика и закрытие им модального окна
+
+modalInner.style.position = 'relative'
+
+const addCloseBtn = () => {
+    const closeBtn = document.createElement('div')
+
+    closeBtn.classList.add('close-button')
+    closeBtn.innerHTML = '&times'
+    closeBtn.style.position = 'absolute'
+    closeBtn.style.right = '10px'
+    closeBtn.style.top = '10px'
+    closeBtn.style.width = '20px'
+    closeBtn.style.height = '20px'
+    closeBtn.style.fontSize = '26px'
+    closeBtn.style.cursor = 'pointer'
+    closeBtn.style.display = 'flex'
+    closeBtn.style.justifyContent = 'center'
+    closeBtn.style.alignItems = 'center'
+
+    modalInner.append(closeBtn)
+
+    closeBtn.addEventListener('click', () => {
+        modal.style.display = ''
+    })
+}
+
+addCloseBtn()
